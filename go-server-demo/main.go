2feb41c7ea6df873a1ebd02e83892d9209445206
dsl2022 <2015rpro@gmail.com>
2025-05-17 15:10:22 -0400

    test changing handler to return different message to validate deployment is successfully setup

--- go-server-demo/main.go
+++ go-server-demo/main.go
@@ -2,18 +2,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, David!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Starting server on :8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
