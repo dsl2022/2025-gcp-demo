e3227b46f25d90039b3e6afc460fb81c9d850531
dsl2022 <2015rpro@gmail.com>
2025-05-17 14:44:42 -0400

    updated to include cicd with github action

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+# output "service_ip" {
+#   description = "External IP of the Hello‑World LoadBalancer"
+#   value       = kubernetes_service.go_hello_world.status[0].load_balancer[0].ingress[0].ip
+# }
